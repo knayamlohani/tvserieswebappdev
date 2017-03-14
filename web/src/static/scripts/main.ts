36cb3245e6e8584196033e6b37f02c9faee0dbcd
Mayank Lohani <mayanklohani@Mayank-Lohanis-MacBook-Pro.local>
2017-03-14 19:18:25 +0530

    enables angular production mode

--- web/src/static/scripts/main.ts
+++ web/src/static/scripts/main.ts
@@ -7,5 +7,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/module/app.module.main';
 import { enableProdMode } from "@angular/core";
 
-// enableProdMode();
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
